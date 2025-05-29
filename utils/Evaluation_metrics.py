39704f6a63b179ce2e7e23ca705b96098c3973ac
mhz133 <46418161+mhz133@users.noreply.github.com>
2025-05-29 20:06:35 +0200

    Update Evaluation_metrics.py

--- utils/Evaluation_metrics.py
+++ utils/Evaluation_metrics.py
@@ -9,6 +9,19 @@ from sklearn.metrics import confusion_matrix
 import numpy as np
 
 def eval_metrics(real_labels,pred_labels):
+        """
+    Computes evaluation metrics for binary classification:
+    - Balanced Accuracy
+    - Half Total Error Rate (HTER)
+    
+    Args:
+        real_labels (array-like): True class labels.
+        pred_labels (array-like): Predicted class labels.
+    
+    Returns:
+        balanced_acc (float): Balanced accuracy, the average of sensitivity and specificity.
+        HTER (float): Half Total Error Rate, the average of FAR and FRR.
+    """
         tn, fp, fn, tp = confusion_matrix(np.array(real_labels), np.array(pred_labels)).ravel()
         sensitivity = tp / (tp + fn)
         specificity = tn / (tn + fp)
